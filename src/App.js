eabab2803a08157488f9d5e6519c56553f00f316
MakarCodes <jarek.strupek@wp.pl>
2020-07-29 09:55:30 +0200

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import BookContextProvider from './contexts/BookContext';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <p>Test</p>;
+  return (
+    <div className='App'>
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
+    </div>
+  );
 }
-
 export default App;
